7d609c1cef72407390bfebae89a8be6ef9768e63
Steve Navarro <steve-o_156@hotmail.com>
2019-07-31 16:13:53 -0500

    Limpiando codigo

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,16 +7,9 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-
-
- 	public function home(){
- 		return view('home');
- 	}
-
- 	public function contactos(){
- 		return view('contactos');
- 	}
-
+	public function home(){
+		return view('home');
+	}
  	public function saludo($nombre = "Invitado"){
  		return view('saludo',compact('nombre'));
  	}
